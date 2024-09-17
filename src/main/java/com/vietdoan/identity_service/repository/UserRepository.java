71ab7600e79b309d4b5eeb87552308f4e82ab0c5
VietYoRHa <thaiviet04@gmail.com>
2024-09-17 22:04:19 +0700

    Video #2

---
+++ src/main/java/com/vietdoan/identity_service/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.vietdoan.identity_service.repository;
+
+import com.vietdoan.identity_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
