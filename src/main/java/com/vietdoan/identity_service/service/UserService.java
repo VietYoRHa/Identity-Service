fd1006e17b7e3357aa81a6ee112683887d173e2f
VietYoRHa <thaiviet04@gmail.com>
2024-09-18 09:59:27 +0700

    Video #4

--- src/main/java/com/vietdoan/identity_service/service/UserService.java
+++ src/main/java/com/vietdoan/identity_service/service/UserService.java
@@ -3,6 +3,8 @@ package com.vietdoan.identity_service.service;
 import com.vietdoan.identity_service.dto.request.UserCreationRequest;
 import com.vietdoan.identity_service.dto.request.UserUpdateRequest;
 import com.vietdoan.identity_service.entity.User;
+import com.vietdoan.identity_service.exception.AppException;
+import com.vietdoan.identity_service.exception.ErrorCode;
 import com.vietdoan.identity_service.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,7 +20,7 @@ public class UserService {
         User user = new User();
 
         if (userRepository.existsByUsername(request.getUsername())) {
-            throw new RuntimeException("User existed.");
+            throw new AppException(ErrorCode.USER_EXISTED);
         }
 
         user.setUsername(request.getUsername());
